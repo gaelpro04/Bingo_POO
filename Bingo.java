e72ed16be0d0c7eae9336147289df9f9603196e0
Gael <gael.lopez22@uabc.edu.mx>
2024-11-22 18:16:13 -0700

    act bingo

--- Bingo.java
+++ Bingo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bingo implements Juego {
 
@@ -6,6 +7,7 @@ public class Bingo implements Juego {
     private Jugador jugador;
     private Tombola tombola;
     private CartaPatrones cartaPatrones;
+    private Carta cartaPatronSeleccionada;
 
     public Bingo()
     {
@@ -13,16 +15,34 @@ public class Bingo implements Juego {
         cartaPatrones = new CartaPatrones();
     }
 
-    public void despliegarMenuPatrones()
+    private Carta seleccionarPatronCarta()
     {
         ArrayList<Carta> patronesCartas = cartaPatrones.getCartaPatrones();
+        Scanner sc = new Scanner(System.in);
+        int indice;
 
         for (int i = 0; i < patronesCartas.size(); ++i) {
             Carta cartaPatron = patronesCartas.get(i);
-
-
+            System.out.println("[" + i + "]==================\n");
+            cartaPatron.mostrarCartaLogica();
         }
-        System.out.println("Elige el patron de la carta para jugar");
+
+        do {
+            System.out.println("Elige el patron de la carta para jugar");
+             indice = sc.nextInt();
+
+             if (indice < 0 || indice > 38) {
+                 System.out.println("Debes ingresar un indice correcto");
+             }
+
+        } while (indice < 0 || indice > 38);
+
+        return cartaPatrones.getCartaPatrones().get(indice);
+    }
+
+    private void compararCartas(Carta cartaPatron, Carta cartaJugador)
+    {
+
     }
 
 
